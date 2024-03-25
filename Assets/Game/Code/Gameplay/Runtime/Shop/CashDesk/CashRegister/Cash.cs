55706a6103ada129b767251502154dceda941c9d
mdlka <mogilevskiy499@gmail.com>
2024-03-25 16:27:39 +0300

    Move some folders, add other dollars

--- Assets/Game/Code/Gameplay/Runtime/Shop/CashDesk/CashRegister/Cash.cs
+++ Assets/Game/Code/Gameplay/Runtime/Shop/CashDesk/CashRegister/Cash.cs
@@ -4,6 +4,20 @@ namespace YellowSquad.CashierSimulator.Gameplay
 {
     public class Cash : MonoBehaviour
     {
-        [field: SerializeField, Min(0)] public float Value { get; private set; }
+        [SerializeField] private float _value;
+        [Header("Material settings")] 
+        [SerializeField] private MeshRenderer _meshRenderer;
+        [SerializeField] private Vector2 _tilling;
+        [SerializeField] private Vector2 _offset;
+
+        public float Value => _value;
+
+        private void Awake()
+        {
+            var props = new MaterialPropertyBlock();
+            props.SetVector("_MainTex_ST", new Vector4(_tilling.x, _tilling.y, _offset.x, _offset.y));
+            
+            _meshRenderer.SetPropertyBlock(props);
+        }
     }
 }
---
+++ Assets/Game/Code/Gameplay/Runtime/Useful/FaceToCamera.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+namespace YellowSquad.CashierSimulator.Gameplay.Useful
+{
+    internal class FaceToCamera : MonoBehaviour
+    {
+        [SerializeField] private bool _yRotate = true;
+        
+        private Transform _camera;
+
+        private void Awake()
+        {
+            _camera = Camera.main.transform;
+        }
+
+        private void Update()
+        {
+            transform.forward = _camera.forward;
+
+            if (_yRotate)
+                return;
+            
+            transform.localRotation = Quaternion.Euler(transform.localRotation.eulerAngles.x, 0, 
+                    transform.localRotation.eulerAngles.z);
+        }
+    }
+}
