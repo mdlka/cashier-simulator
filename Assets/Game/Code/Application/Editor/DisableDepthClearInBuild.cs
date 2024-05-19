d5caeb2ff9e0cafade223510e5646d5fd98fb7d2
mdlka <mogilevskiy499@gmail.com>
2024-05-19 21:02:34 +0300

    Some optimization

---
+++ Assets/Game/Code/Application/Editor/DisableDepthClearInBuild.cs
@@ -0,0 +1,23 @@
+﻿using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace YellowSquad.CashierSimulator.Application.Editor
+{
+    internal class DisableDepthClearInBuild : IProcessSceneWithReport
+    {
+        public int callbackOrder => 1;
+
+        public void OnProcessScene(Scene scene, BuildReport report)
+        {
+            if (UnityEngine.Application.isPlaying) 
+                return;
+            
+            foreach (GameObject rootGameObject in scene.GetRootGameObjects())
+                foreach (Camera camera in rootGameObject.GetComponentsInChildren<Camera>())
+                    if (camera.clearFlags == CameraClearFlags.Depth)
+                        camera.clearFlags = CameraClearFlags.Nothing;
+        }
+    }
+}
