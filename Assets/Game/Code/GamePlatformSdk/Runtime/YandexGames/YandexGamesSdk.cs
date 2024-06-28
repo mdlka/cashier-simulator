1e40a3f90292a416b300a46c1b5ba9ffcf3ba43a
mdlka <mogilevskiy499@gmail.com>
2024-06-28 22:47:41 +0300

    Update YandexGamesSdk.cs

--- Assets/Game/Code/GamePlatformSdk/Runtime/YandexGames/YandexGamesSdk.cs
+++ Assets/Game/Code/GamePlatformSdk/Runtime/YandexGames/YandexGamesSdk.cs
@@ -1,15 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 
 namespace YellowSquad.GamePlatformSdk
 {
     public class YandexGamesSdk : IGamePlatformSdk
     {
+        private readonly IAdvertisement _advertisement = new YandexGamesAdvertisement();
+        
         public IEnumerator Initialize()
         {
             yield return Agava.YandexGames.YandexGamesSdk.Initialize();
         }
 
         public bool Initialized => Agava.YandexGames.YandexGamesSdk.IsInitialized;
-        public IAdvertisement Advertisement { get; } = new YandexGamesAdvertisement();
+
+        public IAdvertisement Advertisement
+        {
+            get
+            {
+                ThrowIfNotInitialized();
+                return _advertisement;
+            }
+        }
+
+        private void ThrowIfNotInitialized()
+        {
+            if (Initialized == false)
+                throw new InvalidOperationException("Sdk not initialized.\nMake sure you call Initialize() and wait for Initialized == true");
+        }
     }
 }
