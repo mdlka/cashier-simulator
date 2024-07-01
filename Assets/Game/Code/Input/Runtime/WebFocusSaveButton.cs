54c12578211242920cd89e92ce610ac34a57854e
mdlka <mogilevskiy499@gmail.com>
2024-07-01 17:56:04 +0300

    Fix focus in webgl

---
+++ Assets/Game/Code/Input/Runtime/WebFocusSaveButton.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace YellowSquad.CashierSimulator.UserInput
+{
+    internal class WebFocusSaveButton : Button
+    {
+        private InputRouter _inputRouter;
+        
+        public override void OnPointerDown(PointerEventData eventData)
+        {
+            base.OnPointerDown(eventData);
+
+#if UNITY_WEBGL && !UNITY_EDITOR
+            _inputRouter ??= FindObjectOfType<InputRouter>();
+            _inputRouter.SetActiveCursor(false);
+#endif
+        }
+    }
+}
